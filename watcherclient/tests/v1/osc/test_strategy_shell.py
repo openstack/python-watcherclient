0be6832e6b81cf3bec958eea31dd5749b2a4dcb8
Vincent Francoise <Vincent.FRANCOISE@b-com.com>
2016-04-29 12:13:06 +0200

    OpenStackClient plugin for strategy
    In this changeset, I implemented the 'strategy' commands.
    
    Partially Implements: blueprint openstackclient-plugin
    
    Change-Id: I3b2db3d2820961ae9c946ec4c8088bf7c503a433

---
+++ watcherclient/tests/v1/osc/test_strategy_shell.py
@@ -0,0 +1,136 @@
+# -*- encoding: utf-8 -*-
+# Copyright (c) 2016 b<>com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+
+import mock
+import six
+
+from watcherclient.tests.v1.osc import base
+from watcherclient import v1 as resource
+from watcherclient.v1 import resource_fields
+from watcherclient import watcher as shell
+
+STRATEGY_1 = {
+    'uuid': '2cf86250-d309-4b81-818e-1537f3dba6e5',
+    'name': 'basic',
+    'display_name': 'Basic consolidation',
+    'goal_uuid': 'fc087747-61be-4aad-8126-b701731ae836',
+    'created_at': datetime.datetime.now().isoformat(),
+    'updated_at': None,
+    'deleted_at': None,
+}
+
+STRATEGY_2 = {
+    'uuid': 'b20bb987-ea8f-457a-a4ea-ab3ffdfeff8b',
+    'name': 'dummy',
+    'display_name': 'Dummy',
+    'goal_uuid': '407b03b1-63c6-49b2-adaf-4df5c0090047',
+    'created_at': datetime.datetime.now().isoformat(),
+    'updated_at': None,
+    'deleted_at': None,
+}
+
+
+class StrategyShellTest(base.CommandTestCase):
+
+    SHORT_LIST_FIELDS = resource_fields.STRATEGY_SHORT_LIST_FIELDS
+    SHORT_LIST_FIELD_LABELS = (
+        resource_fields.STRATEGY_SHORT_LIST_FIELD_LABELS)
+    FIELDS = resource_fields.STRATEGY_FIELDS
+    FIELD_LABELS = resource_fields.STRATEGY_FIELD_LABELS
+
+    def setUp(self):
+        super(self.__class__, self).setUp()
+
+        p_strategy_manager = mock.patch.object(resource, 'StrategyManager')
+        self.m_strategy_mgr_cls = p_strategy_manager.start()
+        self.addCleanup(p_strategy_manager.stop)
+
+        self.m_strategy_mgr = mock.Mock()
+        self.m_strategy_mgr_cls.return_value = self.m_strategy_mgr
+
+        self.stdout = six.StringIO()
+        self.cmd = shell.WatcherShell(stdout=self.stdout)
+
+    def test_do_strategy_list(self):
+        strategy1 = resource.Strategy(mock.Mock(), STRATEGY_1)
+        strategy2 = resource.Strategy(mock.Mock(), STRATEGY_2)
+        self.m_strategy_mgr.list.return_value = [
+            strategy1, strategy2]
+
+        exit_code, results = self.run_cmd('strategy list')
+
+        self.assertEqual(0, exit_code)
+        self.assertEqual(
+            [self.resource_as_dict(strategy1, self.SHORT_LIST_FIELDS,
+                                   self.SHORT_LIST_FIELD_LABELS),
+             self.resource_as_dict(strategy2, self.SHORT_LIST_FIELDS,
+                                   self.SHORT_LIST_FIELD_LABELS)],
+            results)
+
+        self.m_strategy_mgr.list.assert_called_once_with(detail=False)
+
+    def test_do_strategy_list_detail(self):
+        strategy1 = resource.Strategy(mock.Mock(), STRATEGY_1)
+        strategy2 = resource.Strategy(mock.Mock(), STRATEGY_2)
+        self.m_strategy_mgr.list.return_value = [
+            strategy1, strategy2]
+
+        exit_code, results = self.run_cmd('strategy list --detail')
+
+        self.assertEqual(0, exit_code)
+        self.assertEqual(
+            [self.resource_as_dict(strategy1, self.SHORT_LIST_FIELDS,
+                                   self.SHORT_LIST_FIELD_LABELS),
+             self.resource_as_dict(strategy2, self.SHORT_LIST_FIELDS,
+                                   self.SHORT_LIST_FIELD_LABELS)],
+            results)
+
+        self.m_strategy_mgr.list.assert_called_once_with(detail=True)
+
+    def test_do_strategy_list_filter_by_goal(self):
+        strategy1 = resource.Strategy(mock.Mock(), STRATEGY_1)
+        self.m_strategy_mgr.list.return_value = [strategy1]
+
+        exit_code, results = self.run_cmd(
+            'strategy list --goal-uuid '
+            '770ef053-ecb3-48b0-85b5-d55a2dbc6588')
+
+        self.assertEqual(0, exit_code)
+        self.assertEqual(
+            [self.resource_as_dict(strategy1, self.SHORT_LIST_FIELDS,
+                                   self.SHORT_LIST_FIELD_LABELS)],
+            results)
+
+        self.m_strategy_mgr.list.assert_called_once_with(
+            detail=False,
+            goal_uuid='770ef053-ecb3-48b0-85b5-d55a2dbc6588',
+        )
+
+    def test_do_strategy_show_by_uuid(self):
+        strategy = resource.Strategy(mock.Mock(), STRATEGY_1)
+        self.m_strategy_mgr.get.return_value = strategy
+
+        exit_code, result = self.run_cmd(
+            'strategy show f8e47706-efcf-49a4-a5c4-af604eb492f2')
+
+        self.assertEqual(0, exit_code)
+        self.assertEqual(
+            self.resource_as_dict(strategy, self.FIELDS, self.FIELD_LABELS),
+            result)
+        self.m_strategy_mgr.get.assert_called_once_with(
+            'f8e47706-efcf-49a4-a5c4-af604eb492f2')
---
+++ watcherclient/v1/osc/strategy_shell.py
@@ -0,0 +1,111 @@
+# -*- encoding: utf-8 -*-
+# Copyright (c) 2016 b<>com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from openstackclient.common import utils
+
+from watcherclient._i18n import _
+from watcherclient.common import command
+from watcherclient.common import utils as common_utils
+from watcherclient import exceptions
+from watcherclient.v1 import resource_fields as res_fields
+
+
+class ShowStrategy(command.ShowOne):
+    """Show detailed information about a given strategy."""
+
+    def get_parser(self, prog_name):
+        parser = super(ShowStrategy, self).get_parser(prog_name)
+        parser.add_argument(
+            'strategy',
+            metavar='<strategy>',
+            help=_('UUID or name of the strategy'),
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        client = getattr(self.app.client_manager, "infra-optim")
+
+        try:
+            strategy = client.strategy.get(parsed_args.strategy)
+        except exceptions.HTTPNotFound as exc:
+            raise exceptions.CommandError(str(exc))
+
+        columns = res_fields.STRATEGY_FIELDS
+        column_headers = res_fields.STRATEGY_FIELD_LABELS
+
+        return column_headers, utils.get_item_properties(strategy, columns)
+
+
+class ListStrategy(command.Lister):
+    """List information on retrieved strategies."""
+
+    def get_parser(self, prog_name):
+        parser = super(ListStrategy, self).get_parser(prog_name)
+        parser.add_argument(
+            '--goal-uuid',
+            metavar='<goal_uuid>',
+            dest='goal_uuid',
+            help=_('UUID of the goal'))
+        parser.add_argument(
+            '--detail',
+            dest='detail',
+            action='store_true',
+            default=False,
+            help=_("Show detailed information about each strategy."))
+        parser.add_argument(
+            '--limit',
+            metavar='<limit>',
+            type=int,
+            help=_('Maximum number of strategies to return per request, '
+                   '0 for no limit. Default is the maximum number used '
+                   'by the Watcher API Service.'))
+        parser.add_argument(
+            '--sort-key',
+            metavar='<field>',
+            help=_('Goal field that will be used for sorting.'))
+        parser.add_argument(
+            '--sort-dir',
+            metavar='<direction>',
+            choices=['asc', 'desc'],
+            help='Sort direction: "asc" (the default) or "desc".')
+
+        return parser
+
+    def take_action(self, parsed_args):
+        client = getattr(self.app.client_manager, "infra-optim")
+
+        params = {}
+        if parsed_args.detail:
+            fields = res_fields.STRATEGY_FIELDS
+            field_labels = res_fields.STRATEGY_FIELD_LABELS
+        else:
+            fields = res_fields.STRATEGY_SHORT_LIST_FIELDS
+            field_labels = res_fields.STRATEGY_SHORT_LIST_FIELD_LABELS
+
+        if parsed_args.goal_uuid:
+            params["goal_uuid"] = parsed_args.goal_uuid
+
+        params.update(
+            common_utils.common_params_for_list(
+                parsed_args, fields, field_labels))
+
+        try:
+            data = client.strategy.list(**params)
+        except exceptions.HTTPNotFound as ex:
+            raise exceptions.CommandError(str(ex))
+
+        return (field_labels,
+                (utils.get_item_properties(item, fields) for item in data))
